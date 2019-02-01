29bfea14309a84d22e643686170aa893a09baeb2
Twaha-Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2019-02-01 11:20:11 +0600

    Invalid link detector added

--- src/pages/Add/Add.tsx
+++ src/pages/Add/Add.tsx
@@ -39,77 +39,82 @@ class Add extends React.Component<any> {
     const inputData = inputRef.value;
 
     if (currentStep === 1) {
-      if (inputRef.value !== ``) {
+      if (inputData !== ``) {
         const inputLinkParts = inputData.split(`/`);
-        const ytId = inputLinkParts[inputLinkParts.length - 1];
+        if (inputLinkParts.length === 1) {
+          inputRef.style.borderBottomColor = `red`;
+          inputRef.value = `The link you have entered is not valid!`;
+        } else {
+          const ytId = inputLinkParts[inputLinkParts.length - 1];
 
-        const firstLink = linkGenerator({ id: ytId, type: `channels`, part: `contentDetails` });
-        this.props.dispatch({
-          type: `showLoader`
-        });
-        fetch(firstLink)
-          .then(firstLinkResponse => {
-            firstLinkResponse.json().then(firstLinkData => {
-              if (firstLinkData.items.length === 0) {
-                this.props.dispatch({
-                  type: `showError`
-                });
-              } else {
-                const playlistId: string = firstLinkData.items[0].contentDetails.relatedPlaylists.uploads;
-                const secondLink = linkGenerator({
-                  playlistId,
-                  type: `playlistItems`,
-                  maxResults: 50,
-                  part: `snippet`
-                });
-
-                fetch(secondLink)
-                  .then(secondLinkResponse => {
-                    secondLinkResponse.json().then(secondLinkData => {
-                      const videoInfoArray = secondLinkData.items;
-                      const descriptions: string[] = [];
-                      const thumbnailLinks: string[] = [];
-                      const titles: string[] = [];
-                      videoInfoArray.forEach((abc: any) => {
-                        titles.push(abc.snippet.title);
-                        descriptions.push(abc.snippet.description);
-                        thumbnailLinks.push(abc.snippet.thumbnails.medium.url);
-                      });
-                      multipleStoreCommits({
-                        refToDispatcher: this.props.dispatch,
-                        commitName: `Title`,
-                        arrayToCommit: titles
-                      });
-
-                      multipleStoreCommits({
-                        refToDispatcher: this.props.dispatch,
-                        commitName: `Description`,
-                        arrayToCommit: descriptions
-                      });
-
-                      multipleStoreCommits({
-                        refToDispatcher: this.props.dispatch,
-                        commitName: `ThumbnailLink`,
-                        arrayToCommit: thumbnailLinks
-                      });
-                      this.props.dispatch({
-                        type: `stepInc`
-                      });
-                    });
-                  })
-                  .catch(why => {
-                    this.props.dispatch({
-                      type: `showError`
-                    });
-                  });
-              }
-            });
-          })
-          .catch(reason => {
-            this.props.dispatch({
-              type: `showError`
-            });
+          const firstLink = linkGenerator({ id: ytId, type: `channels`, part: `contentDetails` });
+          this.props.dispatch({
+            type: `showLoader`
           });
+          fetch(firstLink)
+            .then(firstLinkResponse => {
+              firstLinkResponse.json().then(firstLinkData => {
+                if (firstLinkData.items.length === 0) {
+                  this.props.dispatch({
+                    type: `showError`
+                  });
+                } else {
+                  const playlistId: string = firstLinkData.items[0].contentDetails.relatedPlaylists.uploads;
+                  const secondLink = linkGenerator({
+                    playlistId,
+                    type: `playlistItems`,
+                    maxResults: 50,
+                    part: `snippet`
+                  });
+
+                  fetch(secondLink)
+                    .then(secondLinkResponse => {
+                      secondLinkResponse.json().then(secondLinkData => {
+                        const videoInfoArray = secondLinkData.items;
+                        const descriptions: string[] = [];
+                        const thumbnailLinks: string[] = [];
+                        const titles: string[] = [];
+                        videoInfoArray.forEach((abc: any) => {
+                          titles.push(abc.snippet.title);
+                          descriptions.push(abc.snippet.description);
+                          thumbnailLinks.push(abc.snippet.thumbnails.medium.url);
+                        });
+                        multipleStoreCommits({
+                          refToDispatcher: this.props.dispatch,
+                          commitName: `Title`,
+                          arrayToCommit: titles
+                        });
+
+                        multipleStoreCommits({
+                          refToDispatcher: this.props.dispatch,
+                          commitName: `Description`,
+                          arrayToCommit: descriptions
+                        });
+
+                        multipleStoreCommits({
+                          refToDispatcher: this.props.dispatch,
+                          commitName: `ThumbnailLink`,
+                          arrayToCommit: thumbnailLinks
+                        });
+                        this.props.dispatch({
+                          type: `stepInc`
+                        });
+                      });
+                    })
+                    .catch(why => {
+                      this.props.dispatch({
+                        type: `showError`
+                      });
+                    });
+                }
+              });
+            })
+            .catch(reason => {
+              this.props.dispatch({
+                type: `showError`
+              });
+            });
+        }
       } else {
         inputRef.style.borderBottomColor = `red`;
         inputRef.placeholder = `Please fill in this field`;
